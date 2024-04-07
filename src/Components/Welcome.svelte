f51108aebb4eca8586255dea59a72bd30aa9eab4
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-06 21:21:04 -0300

    add react, Player component and Welcome with svelte

---
+++ src/Components/Welcome.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const hours = currentTime.getHours()
+
+  let greeting = ''
+
+  if (hours < 12) {
+    greeting = 'Buenos días'
+  } else if (hours < 19) {
+    greeting = 'Buenas tardes'
+  } else {
+    greeting = 'Buenas noches'
+  }
+</script>
+
+<h1 class="text-3xl font-bold pt-6">{greeting}</h1>
