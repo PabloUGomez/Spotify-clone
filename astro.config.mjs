b4ad11ed060d2300da337dde1d5c5fa1e5f6da7d
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-08 00:51:59 -0300

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
-});
+  output: 'server',
+  adapter: vercelServerless({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
+})
